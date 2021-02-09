f7b6b757b65b6529e0b9a67957919e651d03f118
Ben Stetzer <benstetzer@gmail.com>
2021-02-09 18:51:03 -0500

    stuff n other stuff

--- Employees and Managers/Manager.js
+++ Employees and Managers/Manager.js
@@ -1,24 +1,33 @@
-const Employee = require("./Employee.js")
+const { Employee } = require("./Employee.js")
 
-class Manager extends Employee{
+class Manager extends Employee {
     constructor(name, title, salary, boss, employees){
        super(name, title, salary, boss)
        this.employees = employees;
     }
     totalSubsalary(){
-
-        return
+        let subsalary = 0;
+        employees.reduce((accum, ele) => {
+            Employee.(ele.toLowercase())
+        })
+        
     }
-    bonus(multiplier){
+    // bonus(multiplier){
 
-    }
+    // }
 }
+const hobbes = new Manager("Hobbes", 1000000, "Founder", null, ["Calvin"]);
+const calvin = new Manager("Calvin", 130000, "Director", "Hobbes", ["Susie"]);
+const susie = new Manager("Susie", 100000, "TA Manager", "Calvin", ["Lily", "Clifford"]);
+const lily = new Employee("Lily", 90000, "TA", "Susie");
+const clifford = new Employee("Clifford", 90000, "TA", "Susie");
+
+console.log(Employee)
 
 
-
-// Name	Salary	Title	Boss
-// Hobbes	1000000	Founder	null
+// Name	    Salary	Title	    Boss
+// Hobbes	1000000	Founder	    null
 // Calvin	130000	Director	Hobbes
 // Susie	100000	TA Manager	Calvin
-// Lily	90000	TA	Susie
+// Lily	    90000	TA	Susie
 // Clifford	90000	TA	Susie
